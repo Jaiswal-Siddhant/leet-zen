8d8135ea29c1b0e4cc527cd6a0c66ff2a367f828
Jaiswal-Siddhant <jaiswalsiddhant2001@gmail.com>
2025-12-07 02:26:53 +0530

    chore: cleanup

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig } from 'wxt';
 
-// See https://wxt.dev/api/config.html
 export default defineConfig({
-	modules: ['@wxt-dev/module-react'],
-	manifest: {
-		permissions: ['storage'],
-		content_scripts: [
-			{
-				matches: ['*://leetcode.com/*'],
-				js: ['entrypoints/content.ts'],
-				run_at: 'document_end',
-			},
-		],
-	},
+  modules: ['@wxt-dev/module-react'],
+  manifest: {
+    permissions: ['storage'],
+    browser_specific_settings: {
+      gecko: {
+        id: '{e84d9c91-ca86-4572-9f30-aaf6de990704}',
+        strict_min_version: '109.0',
+        // @ts-ignore - this is mandatory in firefox new extensions
+        data_collection_permissions: {
+          required: ["none"]
+        }
+      },
+    },
+  },
 });
